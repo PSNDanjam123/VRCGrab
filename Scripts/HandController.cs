39f57606225016e219f6411c95378f214cb3802b
Jamie Bailey <psndanjam123@gmail.com>
2023-03-22 10:05:30 +0000

    split into different files

---
+++ Scripts/HandController.cs
@@ -0,0 +1,136 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+using VRC.Udon.Common;
+
+[UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+public class HandController : UdonSharpBehaviour
+{
+    [Header("Info")]
+    [Tooltip("Is the player currently trying to grab")]
+    public bool Grabbing = false;
+    [Tooltip("The currently grabbed game object")]
+    public GameObject GrabbedObject = null;
+    [Header("Scripts")]
+    public PIDController AngularUpPIDController;
+    public PIDController AngularRightPIDController;
+    public PIDController AngularForwardPIDController;
+    [Header("Settings")]
+    public bool LeftHand = false;
+    public float GrabRadius = 0.2f;
+    public LayerMask GrabLayerMask;
+    [Header("Debug")]
+    [Tooltip("Assign an empty game object for local debug testing")]
+    public GameObject DebugGameObject = null;
+    [HideInInspector]
+    public VRCPlayerApi Player;
+    [HideInInspector]
+    public VRCPlayerApi.TrackingData HandTrackingData;
+    void Start()
+    {
+        GrabLayerMask = LayerMask.GetMask("GunHandle");
+        Player = Networking.LocalPlayer;
+        HandTrackingData = Player.GetTrackingData(LeftHand ? VRCPlayerApi.TrackingDataType.LeftHand : VRCPlayerApi.TrackingDataType.RightHand);
+    }
+
+    void FixedUpdate()
+    {
+        if (!Grabbing)
+        {
+            GrabbedObject = null;
+            return;
+        }
+        GrabbedObject = TryGrab();
+        ApplyGrab();
+    }
+
+    public override void InputGrab(bool value, UdonInputEventArgs args)
+    {
+        if (LeftHand && HandType.LEFT == args.handType)
+        {
+            Grabbing = value;
+        }
+        else if (!LeftHand && HandType.RIGHT == args.handType)
+        {
+            Grabbing = value;
+        }
+    }
+
+    void ApplyGrab()
+    {
+        if (GrabbedObject == null)
+        {
+            return;
+        }
+        // TODO other logic for general purpose grabbing instead of just handles
+        ApplyHandleGrab();
+    }
+
+    void ApplyHandleGrab()
+    {
+        var handleTransform = GrabbedObject.transform;
+        var rb = GrabbedObject.transform.root.GetComponent<Rigidbody>();
+        var offset = handleTransform.position - rb.transform.position;
+
+        // position
+        rb.MovePosition(GetHandPosition() - offset);
+        rb.AddForce(-Physics.gravity * rb.mass, ForceMode.Force);
+
+        // rotation
+        var expFor = GetHandRotation() * Vector3.forward;
+        var curFor = rb.transform.forward;
+
+        var expUp = Vector3.Cross(Vector3.Cross(expFor, Vector3.up), Vector3.up);
+        var curUp = Vector3.Cross(Vector3.Cross(curFor, Vector3.up), Vector3.up);
+        var expRight = Vector3.Cross(Vector3.Cross(expFor, Vector3.right), Vector3.right);
+        var curRight = Vector3.Cross(Vector3.Cross(curFor, Vector3.right), Vector3.right);
+        var expForward = Vector3.Cross(Vector3.Cross(expFor, Vector3.forward), Vector3.forward);
+        var curForward = Vector3.Cross(Vector3.Cross(curFor, Vector3.forward), Vector3.forward);
+
+        CorrectHandleAngle(AngularUpPIDController, rb, expUp, curUp);
+        CorrectHandleAngle(AngularRightPIDController, rb, expRight, curRight);
+        CorrectHandleAngle(AngularForwardPIDController, rb, expForward, curForward);
+        rb.AddTorque(-rb.angularVelocity);
+    }
+
+    private void CorrectHandleAngle(PIDController PIDController, Rigidbody rb, Vector3 current, Vector3 expected)
+    {
+        Vector3 correction = PIDController.CorrectionV3(expected, current, Time.deltaTime);
+        correction = Vector3.Cross(current, expected) * correction.magnitude;
+        rb.AddTorque(correction);
+    }
+
+    Vector3 GetHandPosition()
+    {
+        if (DebugGameObject != null)
+        {
+            return DebugGameObject.transform.position;
+        }
+        return HandTrackingData.position;
+    }
+
+    Quaternion GetHandRotation()
+    {
+        if (DebugGameObject != null)
+        {
+            return DebugGameObject.transform.rotation;
+        }
+        return HandTrackingData.rotation;
+    }
+
+    GameObject TryGrab()
+    {
+        Collider[] colliders = Physics.OverlapSphere(GetHandPosition(), GrabRadius, GrabLayerMask, QueryTriggerInteraction.UseGlobal);
+        if (colliders.Length == 0)
+        {
+            return null;
+        }
+        foreach (var collider in colliders)
+        {
+            return collider.gameObject;
+        }
+        return null;
+    }
+}
---
+++ Scripts/PIDController.cs
@@ -0,0 +1,35 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+[UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+public class PIDController : UdonSharpBehaviour
+{
+    public float P = 1.0f;
+    public float I = 0.0f;
+    public float D = 0.1f;
+
+    [HideInInspector]
+    public Vector3 integralV3;
+    [HideInInspector]
+    public Vector3 lastErrorV3;
+
+    public void SetPID(float newP, float newI, float newD)
+    {
+        P = newP;
+        I = newI;
+        D = newD;
+    }
+
+    public Vector3 CorrectionV3(Vector3 expected, Vector3 current, float timeFrame)
+    {
+        var error = (current - expected) * -1;
+        integralV3 += error * timeFrame;
+        var deriv = (error - lastErrorV3) / timeFrame;
+        lastErrorV3 = error;
+        return error * P + integralV3 * I + deriv * D;
+    }
+
+}
